15b192d94dec30ab595588ea2f55707eb4973baa
Prabhu Thangaraj-EXT <prabhu.thangaraj-ext@vsei.com>
2024-12-04 12:00:30 -0500

    for Azure merge

---
+++ terr.py
@@ -0,0 +1,19 @@
+#Control Statements are two type
+#1.Condition ( it will take only one)
+#2. loop statement ( it will loop the statmt)
+
+marks=int(input("Enter the Marks:"))
+if marks > 85 and marks <=100:
+    print("Congrats Grade A");
+elif marks > 60 and marks <=85:
+    print("Grade B+")
+elif marks > 40 and marks <=60:
+    print("Grade B")
+elif (marks > 30 and marks <=40):
+    print("Grade C")
+else:
+    print("Yor are Fail")
+
+print ("---------------- Next prog-----------------------------")
+
+
